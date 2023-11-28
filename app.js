9688de57468a7b3865c7fdab7ae952e9e26dad8a
MajsterWihajster <jaqb.kluk@gmail.com>
2023-11-28 20:46:27 +0100

    28.11.2023 - bug fixing.

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const submit_button = document.querySelector('.submit_button');
 const last_operation = document.querySelector('.last_operation');
-const result = document.querySelector('.result');
-
-
+const result = document.querySelector('.math_input');
 
 const calculate = () => {
-    
     let math_input = document.querySelector('.math_input').value;
 
     let first_number = '';
-    let second_number = '';
     let sign;
-    
-    let signs_counter = 0;
+    let second_number = '';
 
+    let signs_counter = 0;
     
     let new_value = 0;
 
     let let_count = false;
     let count_dots = 0;
-
-    result.innerHTML = '';
+    
     last_operation.innerHTML = '';
 
     is_input_empty(math_input);
+
+    math_input_length = math_input.length;
 
     for(i=0; i<math_input.length; i++) {//making sure, that field doesn't contain any different character
 
@@ -35,7 +32,7 @@ const calculate = () => {
                     let_count = false;
                     break;
                 }
-            }  else if(math_input[i] == '.') {
+            }  else if(math_input[i] == '.' || math_input[i] == ',') {
                 count_dots++;
                 if(count_dots > 2) {
                     alert("To much dots");
@@ -62,7 +59,7 @@ const calculate = () => {
     if(let_count == true)
     {
         for(i=0; i<math_input.length; i++) { 
-            if(math_input[i] == '.' || Number(math_input[i]) || math_input[i] == 0) {
+            if(math_input[i] == '.' || math_input[i] == ',' || Number(math_input[i]) || math_input[i] == 0) {
                 first_number += math_input[i];
                 new_value = i;
                 new_value++;
@@ -75,34 +72,43 @@ const calculate = () => {
         for(j=new_value; j<math_input.length; j++) {
             second_number += math_input[j];
         }
-     
+
+        //in this part of code we've got first, sign and second number
+        //
+
+        first_number = first_number.replace(',','.');
+        second_number = second_number.replace(',','.');        
 
         first_number = parseFloat(first_number);
         second_number = parseFloat(second_number);
+      
 
-        if(sign.length > 0) {
+        if(sign.length > 0 && !isNaN(second_number))  {
             switch (sign) {
                 case '+':
-                    result.innerHTML = first_number + second_number;
+                    result.value = first_number + second_number;
+                    last_correct_operation(first_number, sign, second_number);
                     break;
                 case '-':
-                    result.innerHTML = first_number - second_number;
+                    result.value = first_number - second_number;
+                    last_correct_operation(first_number, sign, second_number);
                     break;
                 case '*':
-                    result.innerHTML = first_number * second_number;
+                    result.value = first_number * second_number;
+                    last_correct_operation(first_number, sign, second_number);
                     break;
                 case '/':
                     if(second_number == 0) {
                         alert('Do not divide by 0!');
                         break;
                     } else {
-                        result.innerHTML = first_number / second_number;
+                        result .value= first_number / second_number;
+
+                        last_correct_operation(first_number, sign, second_number);
                         break;
                     }
                 }
             }
-        last_operation.innerHTML += first_number + sign + second_number;
-
     }
 }
 
@@ -120,6 +126,11 @@ const allowed_signs = (i, math_input) => {
         return false;
     }
 }
+
+const last_correct_operation = (first_number, sign, second_number) => {
+    last_operation.innerHTML += first_number + sign + second_number;
+}
+
 
 submit_button.addEventListener('click', calculate);
 
